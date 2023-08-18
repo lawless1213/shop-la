b31b5c6ac4112ca2f8145e043790df90549c204f
lawless1213 <andruha2103@gmail.com>
2023-08-18 11:06:17 +0300

    blog page

--- src/Components/Blog/PostPage/PostPage.jsx
+++ src/Components/Blog/PostPage/PostPage.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import getApiData from "../../../data/api";
 import s from './PostPage.module.scss';
 import Preloader from "../../UI/Preloader/Preloader";
@@ -26,44 +26,45 @@ const PostPage = () => {
 
 	return (
 		<div className={s.PostPage}>
-				{
-					loaded ? 
-					<div className={s.PostPageItem}>
-						<div className={s.Info}>
-							<div className={s.Tags}>
-								{
-									post.tags.map(tag => <div className={`${s.myTag} myTag f-s6`}>{tag}</div>)
-								}
-							</div>
-							<button className='myLink dark' style={{pointerEvents:'none'}}>
-								<span class="icon material-symbols-outlined">favorite</span>
-								{
-									post.reactions > 0 ?
-										<div className="counter f-caption">{post.reactions}</div>
-									:
-										''
-								}
-							</button>
+			{
+				loaded ? 
+				<div className={s.PostPageItem}>
+					<div className={s.Info}>
+						<Link to='/blog' className="myLink">Back to blog</Link>
+						<div className={s.Tags}>
+							{
+								post.tags.map(tag => <div className={`${s.myTag} myTag f-s6`}>{tag}</div>)
+							}
 						</div>
-						<div className={s.Image}>
-							<span className={`${s.icon} material-symbols-outlined`}>image_not_supported</span>
-							{/* <img src="" alt="" /> */}
-						</div>
-						<div className={`${s.Title} f-s4`}>{post.title}</div>
-						<div className={`${s.Body} f-body`}>{post.body}</div>
+						<button className='myLink dark' style={{pointerEvents:'none'}}>
+							<span class="icon material-symbols-outlined">favorite</span>
+							{
+								post.reactions > 0 ?
+									<div className="counter f-caption">{post.reactions}</div>
+								:
+									''
+							}
+						</button>
 					</div>
-				:
+					<div className={s.Image}>
+						<span className={`${s.icon} material-symbols-outlined`}>image_not_supported</span>
+						{/* <img src="" alt="" /> */}
+					</div>
+					<div className={`${s.Title} f-s4`}>{post.title}</div>
+					<div className={`${s.Body} f-body`}>{post.body}</div>
+				</div>
+			:
+				<Preloader/>
+			}
+			<div className={s.Comments}>
+				<div className='f-s3'>Comments</div>
+				{
+					loadedComments ? 
+						comments.map(comment => <div className={s.Comment}><div className={`${s.User} f-s5`}>{comment.user.username}</div><div className={`${s.Text} f-body`}>{comment.body}</div></div>)
+					:
 					<Preloader/>
 				}
-				<div className={s.Comments}>
-					<div className='f-s3'>Comments</div>
-					{
-						loadedComments ? 
-							comments.map(comment => <div className={s.Comment}><div className={`${s.User} f-s5`}>{comment.user.username}</div><div className={`${s.Text} f-body`}>{comment.body}</div></div>)
-						:
-						<Preloader/>
-					}
-				</div>
+			</div>
 		</div>
 	)
 }
